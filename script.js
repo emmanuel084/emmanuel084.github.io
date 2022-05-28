bcb90f90291409cff3c40511b8208db158dc22d5
emmanuel084 <79658826+emmanuel084@users.noreply.github.com>
2022-05-28 17:40:57 -0400

    1.3

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+/*---------Modo Oscuro----------*/
+
+var darkmode = document.getElementById("darkmode");
+
+darkmode.onclick = function (){
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")){
+        darkmode.src = "darkmode/sun.png";
+    }else{
+        darkmode.src = "darkmode/moon.png";
+    }
+}
+
+
+
+
+/*---------Botón Subir----------*/
+
 const scrollBtn = document.querySelector (".btn-scroll-top"); 
 
 scrollBtn.addEventListener('click', () => {
